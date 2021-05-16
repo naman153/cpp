fb98fc9132382211e38f8de13ec4d092dfeca03e
Naman Gupta <67692721+naman153@users.noreply.github.com>
2021-05-16 12:17:36 +0530

    Leetcode Problemset

---
+++ Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size(), bits = 0;
+        for (int i=0; i < n; ++i){
+            bits |= nums[i];
+        }
+        int res = bits * pow(2, n-1);
+        return res;
+    }
+};
