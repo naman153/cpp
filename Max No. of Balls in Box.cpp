7ddf5461dc8170d585bb61c65ef447c72ae71737
Naman Gupta <153namangupta@gmail.com>
2021-06-11 21:47:58 +0530

    Leetcode Problemset

---
+++ Max No. of Balls in Box.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countBalls(int lowLimit, int highLimit) {
+        vector<int> ans;
+        int max=0;
+        for(int i=lowLimit; i<=highLimit; i++){
+            int n=i;
+            int sum=0;
+            while(n != 0){
+                sum+=int(n%10);
+                n= int(n/10);
+            }
+            ans.push_back(sum);
+        }
+        sort(ans.begin(), ans.end());
+        for(int i=0;i<ans.size(); ){
+            int countn = count(ans.begin(),ans.end(),ans[i]);
+            (max < countn)? max=countn : max=max;
+            i+=countn;
+        }
+        return max;
+    }
+};
