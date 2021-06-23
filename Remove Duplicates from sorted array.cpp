eb5d3396cca84379d514851acae19659356b4012
Naman Gupta <153namangupta@gmail.com>
2021-06-23 15:14:23 +0530

    Leetcode Problemset

---
+++ Remove Duplicates from sorted array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto it = unique(nums.begin(), nums.end());
+        nums.erase(it, nums.end());
+        return nums.size();
+    }
+};
