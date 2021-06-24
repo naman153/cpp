10603cb6fde332b8d2526bac1935fad99a16cbb2
Naman Gupta <153namangupta@gmail.com>
2021-06-24 12:21:50 +0530

    Update Maximum Subarrays.cpp

--- Maximum Subarrays.cpp
+++ Maximum Subarrays.cpp
@@ -12,3 +12,19 @@ public:
         return maxn;
     }
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxn=INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum= max(nums[i], sum+nums[i]);
+            maxn = max(maxn,sum);
+        }
+        return maxn;
+    }
+};
