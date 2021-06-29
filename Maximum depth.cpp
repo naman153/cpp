8fc596cac3bb838a687be56060a58b62daae9a07
Naman Gupta <153namangupta@gmail.com>
2021-06-29 13:02:30 +0530

    Create Maximum depth.cpp

---
+++ Maximum depth.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        int count=0;
+        queue<TreeNode*> temp;
+        temp.push(root);
+        temp.push(NULL);
+        while(!temp.empty()){
+            root=temp.front();
+            temp.pop();
+            if(root == NULL){
+                if(!temp.empty())
+                    temp.push(NULL);
+                count++;
+                
+            }
+            else{
+                if(root->left){
+                    temp.push(root->left);
+                }
+                if(root->right){
+                    temp.push(root->right);
+                }
+            }
+        }
+        return count;
+    }
+};
