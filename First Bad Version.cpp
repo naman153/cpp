006d5a4f5615a0e63cc7e7284f4a6495438c73cc
Naman Gupta <153namangupta@gmail.com>
2021-09-24 21:29:32 +0530

    Leetcode Problemset

---
+++ First Bad Version.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long int left=1;
+        long int right=n;
+        long int mid;
+        while(left<right){
+            mid=(left+right)/2;
+            if(isBadVersion(mid)){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+};
