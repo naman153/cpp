14bf5ce13b5d955f22afacf34be9b88f73f58399
Naman Gupta <153namangupta@gmail.com>
2021-09-26 11:27:29 +0530

    Leetcode Problemset

---
+++ Move Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                count++;
+                nums.erase(nums.begin()+i, nums.begin()+i+1);
+                i--;
+            }
+        }
+        for(int i=0;i<count;i++){
+            nums.push_back(0);
+        }
+    }
+};
